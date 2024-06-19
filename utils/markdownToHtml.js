604e67d448e8896b5ef35d81a48b1eec3788844d
zcbe <151560157+zcbe@users.noreply.github.com>
2024-06-19 19:15:32 +0200

    fix markdowmtohtml

--- utils/markdownToHtml.js
+++ utils/markdownToHtml.js
@@ -1,29 +1,50 @@
 import { remark } from "remark";
 import html from "remark-html";
 
+const addHeadElements = `
+  <meta charset="UTF-8">
+  <link rel="canonical" href="https://projet-12-mon-portfolio.vercel.app/">
+  <meta name="description" content="Zaïm Chamsseddine Benyahia, développeur web à Lille, utilisant html, css, js, react, swagger, api">
+  <meta name="keywords" content="developpeur web, react, site web">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Zaïm Chamsseddine Benyahia</title>
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+  <link rel="dns-prefetch" href="https://fonts.googleapis.com">
+  <link rel="preload" href="https://fonts.googleapis.com/css2?family=Hind:wght@400;500;700&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+  <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Hind:wght@400;500;700&display=swap"></noscript>
+`;
+
+const addStyle = `
+  <style>
+    @import url("https://fonts.googleapis.com/css2?family=Hind:wght@400;500;700&display=swap");
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+    @import "./markdown.css";
+
+    * {
+      font-family: "Hind";
+    }
+  </style>
+`;
+
+const addScripts = `
+  <script defer src="https://example.com/some-non-critical-script.js"></script>
+`;
+
 export default async function markdownToHtml(markdown) {
-  // Définir une meta description pour le contenu HTML généré
-  const metaDescription = "Zaïm Chamsseddine Benyahia, développeur web à Lille, utilisant html,css,js, react,swagger,api";
-  
-  // Utiliser remark et remark-html pour convertir le Markdown en HTML
   const result = await remark().use(html).process(markdown);
-  
-  // Construire le contenu HTML complet avec la balise meta description
-  const htmlContent = `
-    <!DOCTYPE html>
+  return `
     <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <link rel="canonical" href="https://zcbe.github.io/ninacarducci.github.io/">
-      <title>Nina Carducci - Photographe professionnelle à Bordeaux</title>
-      <meta name="description" content="${metaDescription}">
-      <meta name="keywords" content="photographe, services photo, retouche photo,albums photo">
-    </head>
-    <body>
-      ${result.toString()}
-    </body>
+      <head>
+        ${addHeadElements}
+        ${addStyle}
+      </head>
+      <body>
+        ${result.toString()}
+        ${addScripts}
+      </body>
     </html>
   `;
-  
-  return htmlContent;
 }
