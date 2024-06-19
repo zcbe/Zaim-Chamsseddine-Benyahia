7a089adacf0d38d94b2dd3b19f0cbd00fc1b6e17
zcbe <151560157+zcbe@users.noreply.github.com>
2024-06-19 18:44:28 +0200

    add html

--- utils/markdownToHtml.js
+++ utils/markdownToHtml.js
@@ -3,7 +3,7 @@ import html from "remark-html";
 
 export default async function markdownToHtml(markdown) {
   // Définir une meta description pour le contenu HTML généré
-  const metaDescription = "Portfolio de Zaïm Chamsseddine Benyahia";
+  const metaDescription = "Zaïm Chamsseddine Benyahia, développeur web à Lille, utilisant html,css,js, react,swagger,api";
   
   // Utiliser remark et remark-html pour convertir le Markdown en HTML
   const result = await remark().use(html).process(markdown);
@@ -14,7 +14,10 @@ export default async function markdownToHtml(markdown) {
     <html lang="en">
     <head>
       <meta charset="UTF-8">
+      <link rel="canonical" href="https://zcbe.github.io/ninacarducci.github.io/">
+      <title>Nina Carducci - Photographe professionnelle à Bordeaux</title>
       <meta name="description" content="${metaDescription}">
+      <meta name="keywords" content="photographe, services photo, retouche photo,albums photo">
     </head>
     <body>
       ${result.toString()}
